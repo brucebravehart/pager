b69ff2c91336afae627c8d95f6049c72880297db
unknown <rfische@ethz.ch>
2026-04-03 17:15:01 +0200

    manually parsed DB URL

--- server/src/main.rs
+++ server/src/main.rs
@@ -64,9 +64,17 @@ async fn main() {
     let database_url = std::env::var("DATABASE_URL").expect("DATABASE_URL must be set");
     let database_url = "postgresql://neondb_owner:npg_YOrRA0Kt6cfm@ep-quiet-art-alcgs14k.c-3.eu-central-1.aws.neon.tech/neondb";
     println!("format {database_url}");
+
+    let options = sqlx::postgres::PgConnectOptions::new()
+        .host("ep-quiet-art-alcgs14k.c-3.eu-central-1.aws.neon.tech")
+        .username("neondb_owner")
+        .password("npg_YOrRA0Kt6cfm")
+        .database("neondb")
+        .ssl_mode(sqlx::postgres::PgSslMode::Require);
+
     let pool = PgPoolOptions::new()
         .max_connections(5)
-        .connect(&database_url)
+        .connect_with(options)
         .await
         .expect("Failed to connect to DB");
     let state = AppState { db: pool };
